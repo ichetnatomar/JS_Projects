9fafef0546d4191ea8ed00591dfaf5b7bbd6863a
ichetnatomar <chetnatomar005@gmail.com>
2024-11-21 12:31:44 +0530

    push

---
+++ practice/weather/script-test.js
@@ -0,0 +1,6 @@
+const searchBtn = document.querySelector('seacrh-btn');
+
+searchBtn.addEventListener('click', function(){
+ const searchCity =  document.querySelector('search-field');
+ console.log(searchCity);
+})
