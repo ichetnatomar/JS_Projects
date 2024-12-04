334d30360d41dd6ba451815ed1dcc1cc35aea5e7
ichetnatomar <chetnatomar005@gmail.com>
2024-12-04 08:56:03 +0530

    Intro to Hooks, {useState}, how normal JS variables cannot be used to re-render JSX

--- src/App.js
+++ src/App.js
@@ -1,81 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from './Button';
-import Text from './Text';
-
 
 const App = () => {
+  let message = 'Good morning User';
 
-  const data = [
-    { id: 1, text: 'text1' },
-    { id: 2, text: 'text2' },
-    { id: 3, text: 'text3' },
-    { id: 4, text: 'text4' },
-    { id: 5, text: 'text5' },
-  ];
-
-  const handleClickAction = () => {
-    console.log('clicked from parent');
+  const changeMessage = () => {
+    message = 'Good afternoon user';
+    console.log(message);
   }
 
   return <>
-  {
-    data.map((item, index)=><Text key = {index}>{item.text}</Text>) //never use index as a key
-  }
+    <div>{message}</div>
+    <Button clickButton={changeMessage}>{'click me'}</Button>
   </>
-
 }
 
 export default App;
-
-
-
-
-// another way of returnng array values: in return make fragments, and pass entire array of components, within {}. Associate a key with a comp.
-
-// return <>
-// {[
-//   <Text key={data[0].id}>{data[0].text}</Text>,
-//   <Text key={data[1].id}>{data[1].text}</Text>
-// ]}
-// </>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import Button from './Button';
-
-
-// const App = () => {                                  //App is a functional component
-//   const handleClickParent = () => {                  //children method of Button component
-//     console.log('CLicked from inside Parent.');
-//   }
-//   return <Button clickAct={handleClickParent}>click me from App</Button>; //whatever gets passed inside component, becomes children of component.
-// }
-
-// export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,66 +1,16 @@
 import React from 'react';
 
 
-//create Button component
-const Button = (props) => {                                      //props catches childrena and other functions from App
-  const { children, clickAction, data } = props;                       //children: click me from App
+const Button = (props) => {
+  const { children, clickButton } = props;
 
-  const handleClick = () => {
-    clickAction?.();                                             //Optional Chaining, if clickAcvtion is not passed from App, it exectes nothing
-
+  const doSomething = ()=>{
+    console.log('button got clicked');
+    clickButton();
   }
 
-  //JSX
-  return <button onClick={handleClick}>
-    {children}
-    +
-    {data?.a?.d?.d}
-  </button>
+
+  return <button onClick={doSomething}>{children}</button>
 }
 
 export default Button;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-
-// const Button = (props) => {                            //Button is a functional component. messageInsideButton is taken from App.js
-//   console.log(props); //click me, clickact
-//   const handleClick = () => {
-//     clickAct();
-//     console.log('button clicked inside component.');
-//   }
-
-//   const { children, clickAct} = props;                          //{children} property of Button component becomes messageInsideButton.
-//   return <button onClick={handleClick}>{children}</button>   //This is JSX code,which react.createelement. hence it requires react to be imported
-// }
-
-// export default Button;
-
-
