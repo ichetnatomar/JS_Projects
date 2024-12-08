dd5c80edaebf24248fa0194a37d57841742a0ebe
ichetnatomar <chetnatomar005@gmail.com>
2024-12-08 14:16:23 +0530

    Event.stopPropogation() to stop event from being propogated further

--- Event Propogation/script.js
+++ Event Propogation/script.js
@@ -1,28 +1,20 @@
 const childElement = document.querySelector('.child');
 const parentElement = document.querySelector('.parent');
 const grandParentElement = document.querySelector('.grandparent');
-console.log('hi')
 
-childElement.addEventListener('click', () => console.log('child clicked'), false);
-parentElement.addEventListener('click', () => console.log('parent clicked'), false);
-grandParentElement.addEventListener('click', () => console.log('grand parent clicked'), true);
-
-// a few more practice exapmles
-// childElement.addEventListener('click', () => console.log('child clicked'), true);
-// parentElement.addEventListener('click', () => console.log('parent clicked'), false);
-// grandParentElement.addEventListener('click', () => console.log('grand parent clicked'), true);
+//How to stop Event Propogation?
+//On clicking child, you only want child and then parent 's event to be triggered, and STOP after that, not propogate it any further..
+//Use event.stopPropogation() at the last stop.
 
 
-// childElement.addEventListener('click', () => console.log('child clicked'), false);
-// parentElement.addEventListener('click', () => console.log('parent clicked'), false);
-// grandParentElement.addEventListener('click', () => console.log('grand parent clicked'), true);
 
+childElement.addEventListener('click', () => console.log('child clicked'));
 
-// childElement.addEventListener('click', () => console.log('child clicked'), false);
-// parentElement.addEventListener('click', () => console.log('parent clicked'), true);
-// grandParentElement.addEventListener('click', () => console.log('grand parent clicked'), true);
+//add stopPropogation() here
+parentElement.addEventListener('click', (e) => {
+  console.log('parent clicked');
+  e.stopPropagation();
+}
+);
 
-
-//event capturing or event trickling
-// when capture parameter is set to true, and when a target is clicked, execution first moves upwards,
-// towards the topmmost element with true, exectues it event , and then moves inwards, executing events(of elements that have capture:true), till it reaches the target, and finally exectues the target's even
+grandParentElement.addEventListener('click', () => console.log('grand parent clicked'));
