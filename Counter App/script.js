0a2367f81631a2a24cb9a179c1809b921356e786
ichetnatomar <chetnatomar005@gmail.com>
2024-12-09 15:16:32 +0530

    counter app completed

---
+++ Counter App/script.js
@@ -0,0 +1,24 @@
+//on clicking the plus button, it should increment value by 1
+const addBtnElement = document.querySelector('#add-btn');
+let countElement = document.querySelector('#count');
+const subtractBtnElement = document.querySelector('#subtract-btn');
+
+
+console.log(countElement.innerText);
+addBtnElement.addEventListener('click', () => {
+
+  let currCountValue = countElement.innerText;
+  currCountValue++;
+  countElement.innerText = currCountValue;
+
+});
+
+
+subtractBtnElement.addEventListener('click', () => {
+  let currCountValue = countElement.innerText;
+  console.log(currCountValue);
+  currCountValue--;
+  countElement.innerText = currCountValue;
+})
+
+
