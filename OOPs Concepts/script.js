f38ffc946b4281a34efa9d6f45766e8431fd05a7
ichetnatomar <chetnatomar005@gmail.com>
2024-12-20 17:30:38 +0530

    Object.create, Prototype object

--- OOPs Concepts/script.js
+++ OOPs Concepts/script.js
@@ -71,6 +71,7 @@ const newPerson = function (name, age, gender) {
 
 const person3 = new newPerson('Deepak', 35, 'male');
 console.log(person3);
+console.log(person3.hasOwnProperty('printDetails')); //true, for each object a separate printDetails() is created
 
 const person4 = new newPerson('amit', 35, 'male');
 console.log(person4);
@@ -97,6 +98,7 @@ class NewPerson {
 //create NewPerson object
 const newPerson1 = new NewPerson('Sobia', 38, 'Female');
 console.log(newPerson1);
+console.log(newPerson1.hasOwnProperty('printDetails')); //false, since printDetails is inside constructor prototype
 
 
 const newPerson2 = new NewPerson('Ranjita', 38, 'Female');
@@ -132,7 +134,101 @@ console.log(newPersonConst2);
 
 const newPersonConst3 = new newPersonConst('Stewart', 21, 'M');
 console.log(newPersonConst3);
+console.log(newPersonConst3.hasOwnProperty('printDetails'));
 
+const Car = function (name, brand, type) {
+  this.name = name;
+  this.brand = brand;
+  this.type = type;
+
+  this.printDetails = function () {
+    console.log(`${this.name} , ${this.brand}, ${this.type}`);
+  }
+}
+
+const car1 = new Car("swift", 'Maruti', 'hatchback');
+console.log(car1);
+console.log(car1.printDetails);
+
+
+
+
+
+
+
+
+
+
+
+//Object.create
+
+let personPrototype = {
+  printDetails: function () {
+    console.log(`${this.name} is ${this.age} years old, and is a ${this.gender}`);
+  }
+}
+
+const p1 = Object.create(personPrototype);
+p1.name = 'barbie';
+p1.age = 23;
+p1.gender = 'female';
+
+p1.printDetails();  //barbie is 23 years old, and is a female
+console.log(p1);
+// console.log(p1.anything()); //using prototype chaining, anything() is searchd in prototypes, when not found, hits null container, gives error: anythin() is not defined
+
+class Animal {
+  constructor(name, species, type) {
+    this.name = name;
+    this.species = species;
+    this.type = type;
+  }
+
+  printDetails() {
+    console.log(`${this.name} belongs to ${this.species} and is a ${this.type}}`);
+  }
+
+  printDetails2() {
+    console.log(`${this.name} is a ${this.type}}`);
+  }
+}
+
+const lion = new Animal('lion', 'leo', 'carnivore');
+const tiger = new Animal('tiger', 'cats', 'carnivore');
+const deer = new Animal('deer', 'cervidae', 'herbivore');
+console.log(lion);
+
+
+//now do smae thing using constructor functions, and no class
+
+const Animal2 = function (name, species, type) {
+  this.name = name;
+  this.species = species;
+  this.type = type;
+
+  // this.printDetails = () => {
+  //   console.log(`${this.name} belongs to ${this.species} and is a ${this.type}}`);
+  // }
+
+}
+
+const zebra = new Animal2('zebra', 'Hippotigris', 'herbivore');
+const dog = new Animal2('tiger', 'canis', 'carnivore');
+
+console.log(zebra);
+console.log(dog);
+
+//manipulate constuctor object, insert printDetails() function inside it, instaed of making a new printDetails() object each time for different objects
+
+Animal2.prototype.printDetails = () => {
+  console.log(`${this.name} belongs to ${this.species} and is a ${this.type}}`);
+}
+
+
+//now manipulate global object(NEVE DO THIS)
+// Animal2.prototype.__proto__.printDetails = () => {
+//   console.log(`${this.name} belongs to ${this.species} and is a ${this.type}}`);
+// }
 
 
 
