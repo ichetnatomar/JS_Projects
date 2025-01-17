3f72d51d0b5fe975f1aca6fb74b527ba882bfa63
ichetnatomar <chetnatomar005@gmail.com>
2025-01-17 11:43:23 +0530

    Starting with REACT: Functional Component

--- REACT/script.js
+++ REACT/script.js
@@ -1,6 +1,37 @@
+//Traditional way: 
+//step 1: mark your root element
+const rootElement = document.querySelector('#root');
+
+//step 2: make your desired react element
+const headingElement = React.createElement('h1', { className: 'heading1', style: { color: 'pink', backgroundColor: 'skyblue' } }, "Hello from React");
+
+//Step 3: render react element to your root using reactdom
+ReactDOM.render(headingElement, rootElement);
+
+
+
+
+
+
+
+//Functional Component:
+// functional component in jSX = JS + HTML, browser cannot understand jsx, so jsx code is first transcompiled by babel then sent to html so it may understand
+const App = function () {
+  return <h1 className="heading1" style={{color: 'pink', backgroundColor: 'grey', fontSize:'3rem',}}>Hello from react with jsx</h1>;
+}
+
+//rendering functional component called App
+ReactDOM.render(<App />, rootElement);
+
+
+
+
+
+
+//JS way
+
 // document.addEventListener('DOMContentLoaded', ()=>{
 //   const rootElement = document.querySelector('#root');
-//   console.log(rootElement);
 
 //   const helloElement = document.createElement('div');
 //   helloElement.innerText = 'hello';
@@ -8,13 +39,4 @@
 //   rootElement.appendChild(helloElement);
 // });
 
-
-//step 1: mark your root element
-const rootElement = document.querySelector('#root');
-
-//step 2: make your desired react element
-const headingElement = React.createElement('h1', null, "Hello from React");
-
-//Step 3: render react element to your root using reactdom
-ReactDOM.render(headingElement, rootElement);
 
