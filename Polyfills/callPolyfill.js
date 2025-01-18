35dd9266c1bca7bf39d150ddab4e8e3570fe3e64
ichetnatomar <chetnatomar005@gmail.com>
2025-01-18 16:38:45 +0530

    Call Polyfill with Symbol and return value

--- Polyfills/callPolyfill.js
+++ Polyfills/callPolyfill.js
@@ -1,3 +1,5 @@
+const { ConcatenationScope } = require("webpack");
+
 function printDetails(obj) {
   console.log(`I am ${this.name}, age is ${this.age}`);
 }
@@ -13,12 +15,28 @@ const person1 = {
 // printDetails.call(person1);
 
 //Call Polyfill
-Function.prototype.myCall = function (context = {}, ...args) {
+Function.prototype.myCall = function(context = {}, ...args){
+  if(typeof this === 'function'){
 
-  context.myFunction = this;
-  context.myFunction();
-  delete context.myFunction;
+    //handle Primitives
+    context = Object(context);
 
+    //use Symbol to handle property naming conflicts
+    const symbol = Symbol();
+
+    //add function to context
+    context[symbol] = this;
+
+    //call function and store returned value in variable
+    const ans = context[symbol](...args);
+
+    //perform cleanup:remove added function prperty from context 
+    delete context[symbol];
+
+    //return ans
+    return ans;
+  }
 }
 
 printDetails.myCall(person1);
+printDetails.myCall(1);
