ff41de3e59f9e6913fa1a648d63cfb1f156942ca
ichetnatomar <chetnatomar005@gmail.com>
2025-02-01 10:50:32 +0530

    routing using BrowserRouter, Routes, Route of react-router-dom and Link component

--- REACT/IMDb clone/vite-project/src/components/Navbar.jsx
+++ REACT/IMDb clone/vite-project/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 
+import { Link } from 'react-router-dom';
 import logo from '../assets/movie-logo.png';
+
 
 const Navbar = () => {
   return (
     <div className='flex space-x-8 pl-3 py-4 items-center'>
       <img className='w-[70px], h-[70px]' src={logo} alt="logo" />
-      <a href="/" className='text-3xl text-blue-500 font-bold'>Movies</a>
-      <a href="/watchlist" className='text-3xl text-blue-500 font-bold'>Watchlist</a>
-      <a href="/recommend" className='text-3xl text-blue-500 font-bold'>Movie Recommendations AI</a>
+      <Link to="/" className='text-3xl text-blue-500 font-bold'>Movies</Link>
+      <Link to="/watchlist" className='text-3xl text-blue-500 font-bold'>Watchlist</Link>
+      <Link to="/recommend" className='text-3xl text-blue-500 font-bold'>Movie Recommendations AI</Link>
     </div>
   )
 }
